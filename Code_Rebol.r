aaa4016bcec45361a2dd5465468a7e405d0aea2c
valnet91 <jeanjacques.valognes@gmail.com>
2018-06-10 07:32:00 +0200

    Carl Sassenrath créateur de Rebol

--- Code_Rebol.r
+++ Code_Rebol.r
@@ -1,4 +1,5 @@
 Rebol[Auteur: JJV]
 
+Salutation au créateur de Rebol en 1996. La France Occupe une place importante aux côtés de Rebol.
 Carl Sassenrath
 En 1996, il lance son propre langage informatique, Rebol (Relative Expression Based Object Language), qu'il considère comme l'évolution ultime de la méthodologie du langage à balises de type XML2. En 1998, il fonde la société Rebol technologies. En octobre 2012, Rebol devient open source3. 
